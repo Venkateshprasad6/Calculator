46f0f0d81988016dea95dc4b88b549725d6d79cf
venkatesh prasad <prasad99vicky@gmail.com>
2023-12-15 11:15:33 +0530

    calculator project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Calculator from "./Components/Calculator"
+function App() {
+  return <div className="App">
+    <Calculator/>
+  </div>;
+}
+
+export default App;
